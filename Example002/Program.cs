34e288fd42633f580aa5f27974e7924c9b774b28
Artem Ermilov <ermilo-artjom@rambler.ru>
2022-07-07 17:04:26 +0300

    Решение задач

---
+++ Example002/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+Задача 2: Задайте значения M и N. Напишите программу, которая найдёт сумму
+натуральных элементов в промежутке от M до N с помощью рекурсии.
+M = 1; N = 15 -> 120
+M = 4; N = 8 -> 30
+*/
+
+
+int Prompt(string message) // Ввод числа ( функция )
+{
+    Console.Write(message);
+    string a_String = Console.ReadLine();
+    return int.Parse(a_String);
+}
+
+int PrintSumlNumbers(int numberM, int numberN)
+{
+    if (numberM > numberN) return 0;
+    
+    return PrintSumlNumbers(numberM, numberN-1)+numberN;
+
+    
+}
+
+int numberM = Prompt("Введите число 1: ");
+int numberN = Prompt("Введите число 2: ");
+
+System.Console.WriteLine();
+
+int a = PrintSumlNumbers(numberM, numberN);
+System.Console.WriteLine($"Произведение натуральных чисел от {numberM} до {numberN} равняется => {a}" );
